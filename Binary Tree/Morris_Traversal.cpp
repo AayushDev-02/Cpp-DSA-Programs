8b3f45e2dd4fad88a082103c7e50140b25489922
Aayush Yadav <yadavaayush002@gmail.com>
2022-12-17 17:49:00 +0530

    17/12/22

---
+++ Binary Tree/Morris_Traversal.cpp
@@ -0,0 +1,62 @@
+/* ALGORITM
+1. Initialize current as root 
+2. While current is not NULL
+   If the current does not have left child
+      a) Print current’s data
+      b) Go to the right, i.e., current = current->right
+   Else
+      a) Find rightmost node in current left subtree OR
+              node whose right child == current.
+         If we found right child == current
+             a) Update the right child as NULL of that node whose right child is current
+             b) Print current’s data
+             c) Go to the right, i.e. current = current->right
+         Else
+             a) Make current as the right child of that rightmost 
+                node we found; and 
+             b) Go to this left child, i.e., current = current->left
+
+*/
+
+
+
+void MorrisTraversal(struct tNode* root)
+{
+    struct tNode *current, *pre;
+ 
+    if (root == NULL)
+        return;
+ 
+    current = root;
+    while (current != NULL) {
+ 
+        if (current->left == NULL) {
+            cout << current->data << " ";
+            current = current->right;
+        }
+        else {
+ 
+            /* Find the inorder predecessor of current */
+            pre = current->left;
+            while (pre->right != NULL
+                   && pre->right != current)
+                pre = pre->right;
+ 
+            /* Make current as the right child of its
+               inorder predecessor */
+            if (pre->right == NULL) {
+                pre->right = current;
+                current = current->left;
+            }
+ 
+            /* Revert the changes made in the 'if' part to
+               restore the original tree i.e., fix the right
+               child of predecessor */
+            else {
+                pre->right = NULL;
+                cout << current->data << " ";
+                current = current->right;
+            } /* End of if condition pre->right == NULL */
+        } /* End of if condition current->left == NULL*/
+    } /* End of while */
+}
