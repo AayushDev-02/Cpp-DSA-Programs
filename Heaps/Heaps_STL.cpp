c0c85dcd24e177b51ae19f274de8e736e299372d
Aayush Yadav <yadavaayush002@gmail.com>
2023-06-12 21:39:10 +0530

    min heap stl

--- Heaps/Heaps_STL.cpp
+++ Heaps/Heaps_STL.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int main(){
 
+    cout << "Max Heap : " << endl << endl;
 
     // priority queue
     priority_queue<int> pq;     // max heap 
@@ -25,10 +26,28 @@ int main(){
     else 
         cout << "The max heap is not empty" << endl;
 
+    cout << endl;
+
+    cout << "Min Heap" << endl << endl;
 
     priority_queue< int, vector<int>, greater<int> > minHeap;       //syntax for min heap
 
+    minHeap.push(3);
+    minHeap.push(6);
+    minHeap.push(9);
+    minHeap.push(4);
+    minHeap.push(8);
 
+    cout << "Top element: " << minHeap.top() << endl ;
+    cout << "Size of the priority queue is: " << minHeap.size() << endl;
+    minHeap.pop();
+    cout << "Top element: " << minHeap.top() << endl ;
+    cout << "Size of the priority queue is: " << minHeap.size() << endl;
+
+    if(minHeap.empty())
+        cout << "The max heap is empty" << endl;
+    else 
+        cout << "The max heap is not empty" << endl;
 
     return 0;
 }
