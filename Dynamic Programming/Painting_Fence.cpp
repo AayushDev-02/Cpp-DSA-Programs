c1ea8c7eb4e9a961040beb6763866f5e3d9660da
Aayush Yadav <yadavaayush002@gmail.com>
2023-06-27 11:06:09 +0530

    painting fence - topdown and tablulation

--- Dynamic Programming/Painting_Fence.cpp
+++ Dynamic Programming/Painting_Fence.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int solveUsingRecursion(int n, int k){
@@ -18,13 +19,50 @@ int solveUsingRecursion(int n, int k){
     return ans;
 }
 
+int topDownApproach(int n, int k, vector<int> &dp){
+    //base case
+    if(n == 1){ // only 1 fence - thus we can paint in k colors 
+        return k;
+    }
+
+    if(n == 2){
+        return ( k + k * (k-1) ); 
+    }
+
+    if(dp[n] != -1) return dp[n];
+
+
+    dp[n] = (topDownApproach(n-2,k, dp) + topDownApproach(n-1,k, dp)) * (k-1);
+
+    return dp[n];   
+}
+
+//Tabular method
+int bottomUpApproach(int n, int k){
+
+    vector<int> dp(n+1, 0);
+
+    dp[1] = k;
+    dp[2] = ( k + k * (k-1) );
+
+    for(int i=3; i<=n; i++){
+        dp[i] = (dp[i-2] + dp[i-1]) * (k-1);
+    }
+
+    return dp[n];
+}
 
 int main(){
 
-    int n =4;
+    int n = 4;
     int k = 3;
 
-    int ans = solveUsingRecursion(n, k);
+    // int ans = solveUsingRecursion(n, k);
+
+    // vector<int> dp(n+1, -1);
+    // int ans = topDownApproach(n, k, dp);
+
+    int ans = bottomUpApproach(n,k);
 
     cout << ans << endl;
 
