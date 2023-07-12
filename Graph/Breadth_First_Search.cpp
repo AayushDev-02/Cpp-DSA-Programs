1306458d06b59230f1b3a920a8e3dd1d4566fd07
Aayush Yadav <yadavaayush002@gmail.com>
2023-07-12 11:27:36 +0530

    BFS

---
+++ Graph/Breadth_First_Search.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <unordered_map>
+#include <bits/stdc++.h>
+#include <List>
+using namespace std;
+template <typename T>
+
+class Graph {
+    public: 
+        unordered_map<T, list<T>> adjList;
+
+        void addEdge(int u, int v, bool direction) {
+
+            // if direction = 0 --> undirected graph
+            // if direction = 1 --> directed graph
+
+            //in both cases u->v edge to banti hi banti h
+            adjList[u].push_back(v);    // create an edge from u to v
+
+            if(direction == 0){
+                //undirected edge
+                adjList[v].push_back(u);  // since edge is undirected v to u edge also exist;
+            }
+        } 
+
+        void printAdjacencyList() {
+            for(auto node: adjList){
+
+                cout << node.first << "-> ";
+
+                for(auto neighbour: node.second){
+                    cout << neighbour << ", ";
+                }
+
+                cout << endl;
+            }
+        }
+
+
+        //BFS
+        void bfs(T src) {
+            queue<T> q;
+            unordered_map<T, bool> visited;
+
+            //insert the first node and mark it visited
+            q.push(src);
+            visited[src] = true;
+
+            while(!q.empty()){
+                //find the front node
+                T frontNode = q.front();
+                q.pop();
+
+                //print the front node
+                cout << frontNode << ", ";
+
+                //insert the neightbour
+                for(auto neighbour: adjList[frontNode]){
+                    if(!visited[neighbour]){
+                        q.push(neighbour);
+                        visited[neighbour] = true;
+                    }
+                }
+            }
+
+        }
+};
+
+int main(){
+
+    Graph<int> g;
+
+    g.addEdge(0,1,0);
+    g.addEdge(0,2,0);
+    g.addEdge(1,3,0);
+    g.addEdge(3,5,0);
+    g.addEdge(3,7,0);
+    g.addEdge(7,6,0);
+    g.addEdge(7,4,0);
+
+    g.printAdjacencyList();
+    cout << endl;
+    g.bfs(0);
+
+    return 0;
+}
+
+//BFS
